ea2646b004f34dcbf88afc8ad0dd3ae78bfb199c
lalit <lalit@inbe.com>
2018-06-18 23:37:23 +0530

    Added the sideDrawer component

---
+++ client/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.css';
+
+const SideDrawer = (props) => (
+    <div className={classes.SideDrawer}>
+        <Logo />
+        <nav>
+            <NavigationItems />
+        </nav>
+    </div>
+)
+
+export default SideDrawer;
