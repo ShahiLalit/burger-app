302bdbe3579848f4f77cf4a74f5b613a8d2dcfb4
lalit <lalit@inbe.com>
2018-06-19 23:24:44 +0530

    added closing transition to the sideDrawer on clicking outside the sideDrawer

--- client/src/components/Navigation/SideDrawer/SideDrawer.js
+++ client/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -5,18 +5,25 @@ import classes from './SideDrawer.css';
 import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 
-const SideDrawer = (props) => (
-    <Aux>
-        <Backdrop show={props.open} clicked={props.closed}/>
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
+const SideDrawer = (props) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close].join(' ')
+
+    if(props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open].join(' ')
+    }
+    return (
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed} />
+            <div className={attachedClasses}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
-    </Aux>
-)
+        </Aux>
+    )
+}
 
 export default SideDrawer;
